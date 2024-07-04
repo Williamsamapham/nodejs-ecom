616e48da164ac377c8286eeed83abde22357f351
Williamsamapham <139889577+Williamsamapham@users.noreply.github.com>
2024-07-04 13:09:30 +0700

    Filtering  & Sorting

--- server/controller/product.js
+++ server/controller/product.js
@@ -1,6 +1,7 @@
-const Product = require('../model/product')
+const Product = require('../model/product');
 const asyncHandler = require('express-async-handler');
-const slugify = require('slugify')
+const slugify = require('slugify');
+const { options } = require('../routes/product');
 const createProduct = asyncHandler(async (req, res) => {
     if (Object.keys(req.body).length === 0) throw new Error('Missing Inputs')
     if (req.body && req.body.title) req.body.slug = slugify(req.body.title)
@@ -22,13 +23,52 @@ const getProduct = asyncHandler(async (req, res) => {
 })
 // Filtering, sorting & pagination
 const getProducts = asyncHandler(async (req, res) => {
-    const product = await Product.find()
-    return res.status(200).json({
-        success: product ? true : false,
-        product: product ? product : 'Ko tim thay san pham nao'
+    try {
+        const queries = { ...req.query };
+        // Tách các trường đặc biệt ra khỏi query 
+        const excludeFields = ['sort', 'page', 'limit', 'fields'];
+        excludeFields.forEach(el => delete queries[el]);
 
-    })
-})
+        // Format lại các operator cho đúng cú phap mongoose
+        let queryString = JSON.stringify(queries);
+        queryString = queryString.replace(/\b(gte|gt|lt|lte)\b/g, matchedEl => `$${matchedEl}`);
+        const formattedQueries = JSON.parse(queryString);
+
+        // Filtering
+        if (queries?.title) formattedQueries.title = { $regex: queries.title, $options: 'i' };
+        let queryCommand = Product.find(formattedQueries);
+
+        // Sorting
+        // abc, efg => [abc,efg] => abc efg
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            queryCommand = queryCommand.sort(sortBy);
+        }
+
+        // Fields limiting
+
+        // Pagination
+
+        //  Execute query
+        // Số lượng sản phẩm thỏa mãn điều kiện !== số lượng trả về 1 lần gọi API
+        const response = await queryCommand.exec();
+        const counts = await Product.find(formattedQueries).countDocuments();
+        return res.status(200).json({
+            success: true,
+            products: response.length ? response : 'Không tìm thấy sản phẩm',
+            counts
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Đã xảy ra lỗi server',
+            error: error.message
+        });
+    }
+});
+
+
 const updateProduct = asyncHandler(async (req, res) => {
     const { productId } = req.params
     if (req.body && req.body.title) req.body.slug = slugify(req.body.title)
--- server/model/product.js
+++ server/model/product.js
@@ -21,7 +21,7 @@ var productSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    categoty: {
+    category: {
         type: mongoose.Types.ObjectId,
         ref: 'Category',
     },
